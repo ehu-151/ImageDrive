7e72424b6ff8f97f904c4bdd4197b40e92a70b58
ehu <fehuehuf@gmail.com>
2018-03-07 23:29:53 +0900

    docstringの追加

--- PixivDriveConfig.py
+++ PixivDriveConfig.py
@@ -2,30 +2,57 @@ import json
 
 
 class PixivDriveConfig:
+    """
+    PixivDriveのPixivDriveConfig.jsonをパースし、値を取得する為のクラスです。
+    初めに#load_json()でJsonファイルを読み込みます。後は自由に#get_tagsなどでValueを受け取ります。
+    """
     def __init__(self):
         self.tags = None
         self.num_of_image = None
         self.interval = None
 
     def load_json(self, path, encoding):
+        """
+        Jsonファイルを読み込みます。
+        :param path: Jsonファイルのパス
+        :param encoding: Windowsの場合はutf-8_sig
+        :return: なし
+        """
         # ファイルを開く
         f = open(path, 'r', encoding=encoding)
         json_data = json.load(f)
         self.__load_value(json_data)
 
     def __load_value(self, json_data):
+        """
+        Json#load()を引数にとり、フィールドにValueを格納します。
+        :param json_data: Json#load()の返り値
+        :return: なし
+        """
         # 初期化
         self.tags = json_data["want_image"]["tag"]
-        self.num_of_image = json_data["want_image"]["num_of_image"]
-        self.interval = json_data["interval"]
+        self.num_of_image = int(json_data["want_image"]["num_of_image"])
+        self.interval = int(json_data["interval"])
 
     def get_tags(self):
+        """
+        欲しい画像のタグ名を返します。
+        :return: タグ名
+        """
         return self.tags
 
     def get_num_of_image(self):
+        """
+        欲しい画像の数を返します。
+        :return: 枚数
+        """
         return self.num_of_image
 
     def get_interval(self):
+        """
+        PixivDriveの更新間隔を返します。
+        :return: 秒
+        """
         return self.interval
 
 
