88aa5675fbe204b7c02884dc5ed06030e30be8e3
ehu <fehuehuf@gmail.com>
2018-03-07 21:35:16 +0900

    Json()からタグ、画像数、設定ファイル、更新間隔を取得

--- PixivDriveConfig.py
+++ PixivDriveConfig.py
@@ -1,10 +1,33 @@
 import json
 
 
+class PixivDriveConfig:
+    def __init__(self):
+        self.json_data = None
+
+    def load_json(self, path, encoding):
+        f = open(path, 'r', encoding=encoding)
+        self.json_data = json.load(f)
+
+    def get_tags(self):
+        return self.json_data["want_image"]["tag"]
+
+    def get_num_of_image(self):
+        return self.json_data["want_image"]["num_of_image"]
+
+    def get_interval(self):
+        return self.json_data["interval"]
+
+
 def main():
-    f = open(r"PixivDriveConfig.json", 'r', encoding="utf-8_sig")
-    json_data = json.load(f)
-    print(json_data["want_image"]["tag"])
+    config = PixivDriveConfig()
+    config.load_json("PixivDriveConfig.json", "utf-8_sig")
+    tags = config.get_tags()
+    print(tags)
+    num_of_image = config.get_num_of_image()
+    print(num_of_image)
+    interval = config.get_interval()
+    print(interval)
 
 
 if __name__ == '__main__':
