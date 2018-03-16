0d75e8e4ecba6e850ebb36d47975a16bba9126f3
ehu <fehuehuf@gmail.com>
2018-03-17 01:01:04 +0900

    TokenのJsonAdapter作成

---
+++ BuilderAccessToken.py
@@ -0,0 +1,17 @@
+class BuilderAccessToken:
+    def __init__(self):
+        self.access_token = None
+        self.refresh_token = None
+
+    def __create_access_token(self):
+        pass
+
+    def get_access_token(self):
+        pass
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ OneDriveTokenJsonAdapter.py
@@ -0,0 +1,31 @@
+from BasePixivDriveJsonAdapter import BasePixivDriveJsonAdapter
+
+class OneDriveTokenJsonAdapter(BasePixivDriveJsonAdapter):
+    def __init__(self):
+        super().__init__()
+        self.access_token = None
+        self.refresh_token = None
+        self.time_stamp=None
+        self.__path = r"..\onedrive_token.json"
+        self.__load_json()
+
+    def __load_json(self):
+        super(OneDriveTokenJsonAdapter, self).load_json(self.__path, "utf-8_sig")
+        self.__load_value(self.json_data)
+
+    def __load_value(self, json_data):
+        # 初期化
+        self.access_token = json_data["access_token"]
+        self.refresh_token = json_data["refresh_token"]
+        self.time_stamp=json_data["time_stamp"]
+
+
+def main():
+    config=OneDriveTokenJsonAdapter()
+    print(config.access_token)
+    print(config.refresh_token)
+    print(config.time_stamp)
+
+
+if __name__ == '__main__':
+    main()
