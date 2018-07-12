9dbf355f71da82db9141276884cf8d5e1cce9617
ehu <fehuehuf@gmail.com>
2018-07-12 16:03:31 +0900

    Twitterの画像取得の実装

--- PixivImagePicker.py
+++ PixivImagePicker.py
@@ -1,4 +1,5 @@
 from pixivpy3 import AppPixivAPI
+import json
 
 
 class PixivImagePicker(AppPixivAPI):
@@ -11,26 +12,38 @@ class PixivImagePicker(AppPixivAPI):
     #download_binary()で画像のurlを指定して画像のバイナリを返します。複数のバイナリが欲しい場合はfor分で回してください。
     """
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str):
         super(PixivImagePicker, self).__init__()
         self.login(username, password)
 
-    def get_image_urls_by_tag(self, tag, times):
+    def get_image_urls_by_tag(self, tag: str, times: int) -> str:
         """
-        tagを指定して画像のurlを返します。
+          tagを指定して画像のurlを返します。
 
-        :param tag: タグ
-        :param times: 指定タグの画像の取得数
-        :return urls: 指定タグの画像のurlリスト
-        """
-        json_result = self.search_illust(word=tag, req_auth=True)
-        # 枚数分だけダウンロード
+          :param tag: タグ
+          :param times: 指定タグの画像の取得数
+          :return urls: 指定タグの画像のurlリスト
+          """
         urls = []
-        for illust in json_result.illusts[:int(times)]:
-            urls.append(illust["image_urls"]["large"])
+        num = 30
+        for i in range(0, int(times / 30) + 1):
+            if i == 0:
+                json_result = self.search_illust(word=tag, req_auth=True)
+            else:
+                next_qs = self.parse_qs(json_result.next_url)
+                if next_qs is None:
+                    return urls
+                # 空白が'+'に変換されてしまうので、ここで空白に置き換え
+                next_qs["word"] = str(next_qs["word"]).replace('+', ' ')
+                json_result = self.search_illust(**next_qs)
+
+            # 枚数分だけダウンロード
+            if times - (i * 30) < 30: num = times - (i * 30)
+            for illust in json_result.illusts[:num]:
+                urls.append(illust["image_urls"]["large"])
         return urls
 
-    def download_binary(self, url, referer='https://app-api.pixiv.net/'):
+    def download_binary(self, url: str, referer='https://app-api.pixiv.net/'):
         """
         指定した画像urlをバイナリで取得します。
 
@@ -45,10 +58,18 @@ class PixivImagePicker(AppPixivAPI):
 
 
 def main():
-    picker = PixivImagePicker("", "")
-    urls = picker.get_image_urls_by_tag("", 3)
+    # 設定ファイル読み込み
+    with open(r"../config.json", "r", encoding='utf-8_sig') as file:
+        json_data = json.load(file)
+
+    pixiv_config = json_data["pixiv_config"]
+    picker = PixivImagePicker(pixiv_config["pixiv_id"], pixiv_config["password"])
+
+    urls = picker.get_image_urls_by_tag("女の子", 3)
     image_binay = picker.download_binary(urls[0])
-    print(image_binay)
+
+    hoge = picker.search_illust("女の子", req_auth=True)
+    print(str(hoge).replace("\'", '\"'))
 
 
 if __name__ == '__main__':
---
+++ TwitterPicker.py
@@ -0,0 +1,76 @@
+"""
+You can get tweet and image using this class.
+
+このクラスはツイートと画像(バイナリ)を検索し、返すクラス
+"""
+import tweepy
+import requests
+import json
+
+
+class TwitterPicker:
+    def __init__(self, consumer_key: str, consumer_secret: str, access_token: str, access_secret: str):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_secret = access_secret
+        self.api = self.setup()
+
+    def get_binary_image_by_word(self, word: str, num: int = 5):
+        """
+        ツイートのidを取得します
+        :param word:
+        :param only_image:
+        :return:
+        """
+
+        public_tweets = self.api.search(word, lang="ja")
+        now_num = 0
+        image_binary = []
+
+        for tweet in public_tweets:
+            tweet_json = json.loads(json.dumps(tweet._json))
+            try:
+                for media in tweet_json["entities"]["media"]:
+                    res = requests.get(media["media_url"], stream=True)
+                    image_binary.append(res.content)
+                    now_num += 1
+                    if (now_num == num): return image_binary
+
+            except KeyError:
+                # mediaがないとき
+                print("no media")
+
+    def get_image_binary(self, image_url):
+        """
+        指定したidから画像をバイナリで取得します。
+        :param image_url:
+        :return:
+        """
+        pass
+
+    def setup(self):
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_secret)
+
+        api = tweepy.API(auth)
+        return api
+
+
+def main():
+    # 設定ファイル読み込み
+    with open(r"../config.json", "r", encoding='utf-8_sig') as file:
+        json_data = json.load(file)
+
+    consumer_key = json_data["twitter_config"]["consumer_key"]
+    consumer_secret = json_data["twitter_config"]["consumer_secret"]
+    access_token = json_data["twitter_config"]["access_token"]
+    access_secret = json_data["twitter_config"]["access_secret"]
+
+    tweetAPI = TwitterPicker(consumer_key, consumer_secret, access_token, access_secret)
+    image_binary = tweetAPI.get_binary_image_by_word("#背景", 10)
+    print(image_binary)
+
+
+if __name__ == '__main__':
+    main()
